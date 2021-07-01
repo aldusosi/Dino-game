80a8addea1ab75f43ddd8d5c2467d22fcdb83a2f
aldusosi <desenho2510@gmail.com>
2021-07-01 10:20:15 -0300

    adicionado sistema de record

--- js.js
+++ js.js
@@ -1,9 +1,15 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector(".background");
-const pontuacao = document.querySelector('.pontos')
+const pontuacao = document.querySelector('.pontos');
+const recordAtual = document.querySelector('.record');
+
 let isJumping = false;
 let position = 0;
 let pontos = 0;
+
+
+let record = Number(localStorage.getItem('record'));
+recordAtual.innerText = `Record ${record}`;
 
 function handleKeyUp(event){
 	if(event.keyCode === 32 && !isJumping){
@@ -53,10 +59,17 @@ function createCactus(){
 			background.removeChild(cactus);
 			pontos += 10;
 			pontuacao.innerText = `${pontos} Pontos`;
+			if(pontos > record){
+				pontuacao.style.color = "green"
+			}
 		}else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
 			//game over
 			clearInterval(leftInterval);
 			document.body.innerHTML = "<h1 class='game-over'>Fim de jogo</h1>";
+			console.log(pontos, record)
+			if(pontos > record){
+				localStorage.setItem('record', pontos);
+			}
 		}else{
 			cactusPosition -= 10;
 			cactus.style.left = cactusPosition + "px";
